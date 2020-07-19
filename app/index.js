b196bd0e6f48cffbd7a026b7ff35046ecca13c99
Justin Wayne <wayne.jch@gmail.com>
2020-07-19 12:33:33 -0400

    tweakage.

--- app/index.js
+++ app/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
-const app = express(); app.get('/', (req, res) => {
+
+// HTTP
+const app = express();
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
-}); const port = 8765;
+});
+
+// Startup
+const port = 8765;
 app.listen(port, () => {
     console.log(`listening http://localhost:${port}`);
-}); 
+});
