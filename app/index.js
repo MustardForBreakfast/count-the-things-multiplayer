aacda2c64d30a637abc5ff28cce3badec2893b0b
Justin Wayne <wayne.jch@gmail.com>
2020-07-19 23:50:36 -0400

    working prototype

--- app/index.js
+++ app/index.js
@@ -1,14 +1,104 @@
 const express = require('express');
 const path = require('path');
+const WebSocketServer = require('websocket').server;
 
-// HTTP
+// Serve static assets (e.g. html) via normal ol' http
 const app = express();
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+app.use(express.static(__dirname + '/../client'));
+
+// Start an http server
+const port = 8765;
+const server = app.listen(port, () => {
+    console.log(`listening http://localhost:${port}`);
 });
 
-// Startup
-const port = 8765;
-app.listen(port, () => {
-    console.log(`listening http://localhost:${port}`);
+// Do the socket dance
+const wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+// Global count state, because this is a potato app.
+let count = 0;
+
+function sendCount(connection) {
+    const resp = {
+        count: count
+    }
+    connection.sendUTF(JSON.stringify(resp));
+}
+
+function sendCountToAll() {
+    const resp = {
+        count: count
+    }
+    wsServer.connections.forEach(
+        conn => {
+            conn.sendUTF(JSON.stringify(resp));
+        }
+    )
+}
+
+function handleMessage(message, connection) {
+    try {
+        const content = JSON.parse(message.utf8Data);
+
+        if(typeof content.value !== 'number') {
+            throw `invlaid message "value": ${content.value}`;
+        }
+
+        if(content.op === 'add'){
+            count += content.value;
+        } else if(content.op === 'subtract'){
+            count -= content.value;
+        } else {
+            throw `unrecognized op: ${content.op}`;
+        }
+        sendCountToAll();
+    }
+    catch(err) {
+        console.log('invlaid message content: ', err)
+    }
+}
+
+function originIsAllowed(origin) {
+    // TODO: put logic here to detect whether the specified origin is allowed.
+    return true;
+}
+
+wsServer.on('request', function (request) {
+    if (!originIsAllowed(request.origin)) {
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
+    }
+
+    try {
+        const connection = request.accept('counter-protocol', request.origin);
+        console.log((new Date()) + ' Connection accepted.');
+
+        connection.on('message', function (message) {
+            try {
+                if (message.type === 'utf8') {
+                    console.log('Received Message: ' + message.utf8Data);
+                    handleMessage(message, connection)
+                } else {
+                    throw `Unsupported message type: ${message.type}`
+                }
+            } catch(err) {
+                console.log('message failure: ', err)
+            }
+        });
+
+        connection.on('close', function (reasonCode, description) {
+            console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+        });
+
+        // Send the initial count to the new connection
+        sendCount(connection);
+    }
+    catch(err) {
+        console.log(`connection failure: ${err}`)
+    }
+
 });
---
+++ client/index.js
@@ -0,0 +1,116 @@
+function initWsClient() {
+    const host = 'ws://localhost:8765/'
+    const client = new WebSocket(host, 'counter-protocol');
+
+    client.onerror = function (err) {
+        console.log('Connection Error: ', err);
+    };
+
+    client.onopen = function () {
+        console.log('WebSocket Client Connected');
+    };
+
+    client.onclose = function () {
+        console.log('counter-protocol Client Closed');
+    };
+
+    client.onmessage = function (e) {
+        if (typeof e.data === 'string') {
+            try{
+                const message = JSON.parse(e.data);
+                const newCount = message.count;
+                render(newCount);
+            }
+            catch(err){
+                console.log('message reception error: ', err)
+            }
+        }
+    };
+
+    return client;
+}
+
+/**
+ * Redraw the count text with the current count value.
+ */
+function renderCount(count) {
+    const $count = document.getElementById("countDisplay");
+    $count.innerText = `${count}`;
+}
+
+/**
+ * Redraw the H1 text with the current count value.
+ *
+ * ...because I'm five years old.
+ */
+function renderHeaderText(count) {
+    const defaultText = 'Count the things.';
+    const secretText = 'Nice.';
+    const $header = document.getElementById("headerText");
+
+    if (count === 69) {
+        $header.innerText = secretText;
+    } else if ($header.innerText !== defaultText) {
+        $header.innerText = defaultText;
+    }
+}
+
+function render(count) {
+    renderCount(count);
+    renderHeaderText(count);
+}
+
+
+/**
+ * Define behavior for the "+" button.
+ */
+function handlePlusClick(client) {
+    const message = {
+        op: 'add',
+        value: 1
+    }
+    client.send(JSON.stringify(message));
+}
+
+/**
+ * Define behavior for the "-" button.
+ */
+function handleMinusClick(client) {
+    const message = {
+        op: 'subtract',
+        value: 1
+    }
+    client.send(JSON.stringify(message));
+}
+
+/**
+ * Initialize the "+" button.
+ */
+function initPlusButton(client) {
+    const $plus = document.getElementById("plusButton");
+    $plus.onclick = (e) => { 
+        e.preventDefault()
+        handlePlusClick(client);
+    };
+}
+
+/**
+ * Initialize the "-" button.
+ */
+function initMinusButton(client) {
+    const $minus = document.getElementById("minusButton");
+    $minus.onclick = (e) => {
+        e.preventDefault()
+        handleMinusClick(client);
+    };
+}
+
+function init() {
+    const client = initWsClient();
+    initPlusButton(client);
+    initMinusButton(client);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    init()
+});
