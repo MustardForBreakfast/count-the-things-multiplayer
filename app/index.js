3493cfba8e18c93ba9d450ff48d53ddfa6a9a9f3
Justin Wayne <wayne.jch@gmail.com>
2020-07-29 22:17:51 -0400

    more consolidation/moving stuff around

--- app/index.js
+++ app/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const path = require('path');
-const ws = require('websocket');
-const { configureWsConnection } = require('./interface');
-
-const WebSocketServer = ws.server;
+const WebSocketServer = require('websocket').server;
+const { initWsConnection } = require('./interface');
 
 // Serve static assets (e.g. html) via normal ol' http
 const app = express();
@@ -16,35 +14,8 @@ const server = app.listen(port, () => {
 });
 
 // Do the socket dance
-const wsServer = new WebSocketServer({
-    httpServer: server
-});
-
-function originIsAllowed(origin) {
-    // TODO: put logic here to detect whether the specified origin is allowed.
-    return true;
-}
-
-function authenticate(){
-    // TODO: put auth logic here.
-    return true;
-}
-
-wsServer.on('request', function (request) {
-    if (!originIsAllowed(request.origin) || !authenticate()) {
-        // Make sure we only accept requests from an allowed origin
-        request.reject();
-        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-        return;
-    }
-
-    try {
-        const connection = request.accept('counter-protocol', request.origin);
-        console.log((new Date()) + ' Connection accepted.');
-        configureWsConnection(wsServer, connection);
-    }
-    catch(err) {
-        console.log(`connection failure: ${err}`)
-    }
-
-});
+initWsConnection(
+    new WebSocketServer({
+        httpServer: server
+    })
+);
