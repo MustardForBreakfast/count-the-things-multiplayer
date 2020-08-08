1b77f18f7a1efb37794eeb24dc2ce97d7532c101
Justin Wayne <wayne.jch@gmail.com>
2020-08-08 09:08:42 -0400

    programmatically set CSP

--- app/index.js
+++ app/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const path = require('path');
 const WebSocketServer = require('websocket').server;
 const { configureWsServer } = require('./init');
+const { expressCspHeader, SELF, NONE } = require('express-csp-header');
 
 // Serve static assets (e.g. html) via normal ol' http
 const app = express();
+app.use(expressCspHeader({
+    directives: {
+        'default-src': [SELF],
+        'script-src': [SELF],
+        'style-src': [SELF],
+        'img-src': [SELF],
+        'worker-src': [NONE],
+        'block-all-mixed-content': true
+    }
+}));
 app.use(express.static(__dirname + '/../client'));
+
 
 // Start an http server
 const port = process.env.PORT || 8765
